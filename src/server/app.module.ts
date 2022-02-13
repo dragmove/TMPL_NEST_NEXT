8b053d6bb3c5fc4dedd13561a6045b94056f9625
hyunseok.kim <hyunseok.kim@42dot.ai>
2022-02-13 13:10:22 +0900

    env: Add ServeStaticModule to serve static files

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,12 +1,17 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { RenderModule } from 'nest-next';
 import Next from 'next';
 import { ApiModule } from './api/api.module';
 import { AppController } from './app.controller';
 import { ViewModule } from './view/view.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'public'),
+    }),
     RenderModule.forRootAsync(
       Next({
         dev: process.env.NODE_ENV !== 'production',
